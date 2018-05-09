5e747e603820740e8364fd99b9c6182ad1e16bb2
Tim Smith <tsmith@chef.io>
2018-05-08 18:07:21 -0700

    Require Chef 12+ and update metadata
    Fix source/issues URLs
    Add chef_version
    Remove specific OS versions. Require what's still supported
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,16 +1,15 @@
 name             'smartmontools'
 maintainer       'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs and configures smartmontools'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.0'
 
-supports 'debian', '>= 7.0'
-supports 'ubuntu', '>= 12.04'
-supports 'centos', '>= 5.7'
-supports 'redhat', '>= 5.7'
-supports 'scientific'
-supports 'oracle'
-source_url 'https://github.com/jtimberman/smartmontools-cookbook' if respond_to?(:source_url)
-issues_url 'https://github.com/jtimberman/smartmontools-cookbook/issues' if respond_to?(:issues_url)
+%w(ubuntu debian redhat centos fedora scientific oracle).each do |os|
+  supports os
+end
+
+chef_version '>= 12' if respond_to?(:chef_version)
+source_url 'https://github.com/sous-chefs/smartmontools'
+issues_url 'https://github.com/sous-chefs/smartmontools/issues'
