456b3ede8b925bcf278c2edb564c4c64e60fc675
Dan Webb <dan.webb@damacus.io>
2019-08-19 20:42:37 +0100

    Auto correct using cookstyle

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
 #
-# Cookbook Name:: smartmontools
+# Cookbook:: smartmontools
 # Attributes:: default
 #
-# Copyright 2011, Joshua Timberman
+# Copyright:: 2011, Joshua Timberman
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,8 @@
 #
-# Cookbook Name:: smartmontools
+# Cookbook:: smartmontools
 # Recipe:: default
 #
-# Copyright 2011-2015, Joshua Timberman
+# Copyright:: 2011-2015, Joshua Timberman
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
